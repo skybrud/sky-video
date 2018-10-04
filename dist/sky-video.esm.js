d27a3c6bcb19362e580d3f8746966f230e817305
Rudi Ørnhøj <rornhoj@gmail.com>
2018-10-04 11:39:10 +0200

    Updated dependencies + dist

--- dist/sky-video.esm.js
+++ dist/sky-video.esm.js
@@ -1,19 +1,19 @@
 var script = {
 	name: 'SkyVideo',
 	props: ['src', 'poster'],
-	data() {
+	data: function data() {
 		return {
 			embedded: false,
 		};
 	},
 	methods: {
-		embed() {
+		embed: function embed() {
 			this.embedded = true;
 		},
 	},
 	computed: {
-		iframeSrc() {
-			let iframeSrc;
+		iframeSrc: function iframeSrc() {
+			var iframeSrc;
 			if (this.src.indexOf('vimeo') !== -1) {
 				iframeSrc = this.src.replace('vimeo.com', 'player.vimeo.com/video');
 			} else if (this.src.indexOf('youtube') !== -1) {
@@ -22,45 +22,40 @@ var script = {
 			iframeSrc += '?autoplay=true&rel=0';
 			return iframeSrc;
 		},
-		iframePoster() {
+		iframePoster: function iframePoster() {
 			if (!this.poster) {
 				console.warn('SkyVideo: There are no poster defined.');
 				return '';
 			}
-			return `background-image: url('${this.poster}')`;
+			return ("background-image: url('" + (this.poster) + "')");
 		},
-		defaultButton() {
+		defaultButton: function defaultButton() {
 			return this.$slots.play === undefined;
 		},
 	},
 };
 
 /* script */
-            const __vue_script__ = script;
-            
+            var __vue_script__ = script;
 /* template */
 var __vue_render__ = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"sky-video"},[_c('div',{on:{"~click":function($event){return _vm.embed($event)}}},[_c('div',{staticClass:"sky-video-click"},[_c('label',[_c('button',{class:['sky-video-play', {'default-button': _vm.defaultButton}]},[_vm._t("play")],2)])]),_vm._v(" "),(_vm.embedded)?_c('div',{staticClass:"sky-video-iframe"},[_c('iframe',{attrs:{"frameborder":"0","allowfullscreen":"allowfullscreen","allow":"autoplay","src":_vm.iframeSrc}})]):_c('div',{staticClass:"sky-video-poster",style:(_vm.iframePoster)})])])};
 var __vue_staticRenderFns__ = [];
 
   /* style */
-  const __vue_inject_styles__ = function (inject) {
-    if (!inject) return
-    inject("data-v-40c55e1d_0", { source: "\n.sky-video{position:relative;width:100%;left:0;right:0;bottom:0;position:absolute\n}\n.sky-video-iframe{top:0;left:0;right:0;bottom:0;z-index:3;position:absolute;height:100%\n}\n.sky-video-poster{top:0;left:0;right:0;bottom:0;z-index:0;position:absolute;background-size:cover;background-position:center;background-repeat:no-repeat\n}", map: undefined, media: undefined });
-
-  };
+  var __vue_inject_styles__ = undefined;
   /* scoped */
-  const __vue_scope_id__ = undefined;
+  var __vue_scope_id__ = undefined;
   /* module identifier */
-  const __vue_module_identifier__ = undefined;
+  var __vue_module_identifier__ = undefined;
   /* functional template */
-  const __vue_is_functional_template__ = false;
+  var __vue_is_functional_template__ = false;
   /* component normalizer */
   function __vue_normalize__(
     template, style, script$$1,
     scope, functional, moduleIdentifier,
     createInjector, createInjectorSSR
   ) {
-    const component = (typeof script$$1 === 'function' ? script$$1.options : script$$1) || {};
+    var component = (typeof script$$1 === 'function' ? script$$1.options : script$$1) || {};
 
     // For security concerns, we use only base name in production mode.
     component.__file = "SkyVideo.vue";
@@ -70,105 +65,15 @@ var __vue_staticRenderFns__ = [];
       component.staticRenderFns = template.staticRenderFns;
       component._compiled = true;
 
-      if (functional) component.functional = true;
+      if (functional) { component.functional = true; }
     }
 
     component._scopeId = scope;
 
-    {
-      let hook;
-      if (style) {
-        hook = function(context) {
-          style.call(this, createInjector(context));
-        };
-      }
-
-      if (hook !== undefined) {
-        if (component.functional) {
-          // register for functional component in vue file
-          const originalRender = component.render;
-          component.render = function renderWithStyleInjection(h, context) {
-            hook.call(context);
-            return originalRender(h, context)
-          };
-        } else {
-          // inject component registration as beforeCreate hook
-          const existing = component.beforeCreate;
-          component.beforeCreate = existing ? [].concat(existing, hook) : [hook];
-        }
-      }
-    }
-
     return component
   }
   /* style inject */
-  function __vue_create_injector__() {
-    const head = document.head || document.getElementsByTagName('head')[0];
-    const styles = __vue_create_injector__.styles || (__vue_create_injector__.styles = {});
-    const isOldIE =
-      typeof navigator !== 'undefined' &&
-      /msie [6-9]\\b/.test(navigator.userAgent.toLowerCase());
-
-    return function addStyle(id, css) {
-      if (document.querySelector('style[data-vue-ssr-id~="' + id + '"]')) return // SSR styles are present.
-
-      const group = isOldIE ? css.media || 'default' : id;
-      const style = styles[group] || (styles[group] = { ids: [], parts: [], element: undefined });
-
-      if (!style.ids.includes(id)) {
-        let code = css.source;
-        let index = style.ids.length;
-
-        style.ids.push(id);
-
-        if (css.map) {
-          // https://developer.chrome.com/devtools/docs/javascript-debugging
-          // this makes source maps inside style tags work properly in Chrome
-          code += '\n/*# sourceURL=' + css.map.sources[0] + ' */';
-          // http://stackoverflow.com/a/26603875
-          code +=
-            '\n/*# sourceMappingURL=data:application/json;base64,' +
-            btoa(unescape(encodeURIComponent(JSON.stringify(css.map)))) +
-            ' */';
-        }
-
-        if (isOldIE) {
-          style.element = style.element || document.querySelector('style[data-group=' + group + ']');
-        }
-
-        if (!style.element) {
-          const el = style.element = document.createElement('style');
-          el.type = 'text/css';
-
-          if (css.media) el.setAttribute('media', css.media);
-          if (isOldIE) {
-            el.setAttribute('data-group', group);
-            el.setAttribute('data-next-index', '0');
-          }
-
-          head.appendChild(el);
-        }
-
-        if (isOldIE) {
-          index = parseInt(style.element.getAttribute('data-next-index'));
-          style.element.setAttribute('data-next-index', index + 1);
-        }
-
-        if (style.element.styleSheet) {
-          style.parts.push(code);
-          style.element.styleSheet.cssText = style.parts
-            .filter(Boolean)
-            .join('\n');
-        } else {
-          const textNode = document.createTextNode(code);
-          const nodes = style.element.childNodes;
-          if (nodes[index]) style.element.removeChild(nodes[index]);
-          if (nodes.length) style.element.insertBefore(textNode, nodes[index]);
-          else style.element.appendChild(textNode);
-        }
-      }
-    }
-  }
+  
   /* style inject SSR */
   
 
@@ -180,11 +85,11 @@ var __vue_staticRenderFns__ = [];
     __vue_scope_id__,
     __vue_is_functional_template__,
     __vue_module_identifier__,
-    __vue_create_injector__,
+    undefined,
     undefined
   );
 
-const defaults = {
+var defaults = {
 	registerComponents: true,
 };
 
@@ -193,7 +98,8 @@ function install(Vue, options) {
 		return;
 	}
 
-	const { registerComponents } = Object.assign({}, defaults, options);
+	var ref = Object.assign({}, defaults, options);
+	var registerComponents = ref.registerComponents;
 
 	if (registerComponents) {
 		// Main component
--- dist/sky-video.ssr.js
+++ dist/sky-video.ssr.js
@@ -5,19 +5,19 @@ Object.defineProperty(exports, '__esModule', { value: true });
 var script = {
 	name: 'SkyVideo',
 	props: ['src', 'poster'],
-	data() {
+	data: function data() {
 		return {
 			embedded: false,
 		};
 	},
 	methods: {
-		embed() {
+		embed: function embed() {
 			this.embedded = true;
 		},
 	},
 	computed: {
-		iframeSrc() {
-			let iframeSrc;
+		iframeSrc: function iframeSrc() {
+			var iframeSrc;
 			if (this.src.indexOf('vimeo') !== -1) {
 				iframeSrc = this.src.replace('vimeo.com', 'player.vimeo.com/video');
 			} else if (this.src.indexOf('youtube') !== -1) {
@@ -26,45 +26,40 @@ var script = {
 			iframeSrc += '?autoplay=true&rel=0';
 			return iframeSrc;
 		},
-		iframePoster() {
+		iframePoster: function iframePoster() {
 			if (!this.poster) {
 				console.warn('SkyVideo: There are no poster defined.');
 				return '';
 			}
-			return `background-image: url('${this.poster}')`;
+			return ("background-image: url('" + (this.poster) + "')");
 		},
-		defaultButton() {
+		defaultButton: function defaultButton() {
 			return this.$slots.play === undefined;
 		},
 	},
 };
 
 /* script */
-            const __vue_script__ = script;
-            
+            var __vue_script__ = script;
 /* template */
 var __vue_render__ = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"sky-video"},[_vm._ssrNode("<div>","</div>",[_vm._ssrNode("<div class=\"sky-video-click\">","</div>",[_vm._ssrNode("<label>","</label>",[_vm._ssrNode("<button"+(_vm._ssrClass(null,['sky-video-play', {'default-button': _vm.defaultButton}]))+">","</button>",[_vm._t("play")],2)])]),_vm._ssrNode(" "+((_vm.embedded)?("<div class=\"sky-video-iframe\"><iframe frameborder=\"0\" allowfullscreen=\"allowfullscreen\" allow=\"autoplay\""+(_vm._ssrAttr("src",_vm.iframeSrc))+"></iframe></div>"):("<div class=\"sky-video-poster\""+(_vm._ssrStyle(null,_vm.iframePoster, null))+"></div>")))],2)])};
 var __vue_staticRenderFns__ = [];
 
   /* style */
-  const __vue_inject_styles__ = function (inject) {
-    if (!inject) return
-    inject("data-v-40c55e1d_0", { source: "\n.sky-video{position:relative;width:100%;left:0;right:0;bottom:0;position:absolute\n}\n.sky-video-iframe{top:0;left:0;right:0;bottom:0;z-index:3;position:absolute;height:100%\n}\n.sky-video-poster{top:0;left:0;right:0;bottom:0;z-index:0;position:absolute;background-size:cover;background-position:center;background-repeat:no-repeat\n}", map: undefined, media: undefined });
-
-  };
+  var __vue_inject_styles__ = undefined;
   /* scoped */
-  const __vue_scope_id__ = undefined;
+  var __vue_scope_id__ = undefined;
   /* module identifier */
-  const __vue_module_identifier__ = "data-v-40c55e1d";
+  var __vue_module_identifier__ = "data-v-40c55e1d";
   /* functional template */
-  const __vue_is_functional_template__ = false;
+  var __vue_is_functional_template__ = false;
   /* component normalizer */
   function __vue_normalize__(
     template, style, script$$1,
     scope, functional, moduleIdentifier,
     createInjector, createInjectorSSR
   ) {
-    const component = (typeof script$$1 === 'function' ? script$$1.options : script$$1) || {};
+    var component = (typeof script$$1 === 'function' ? script$$1.options : script$$1) || {};
 
     // For security concerns, we use only base name in production mode.
     component.__file = "SkyVideo.vue";
@@ -74,99 +69,17 @@ var __vue_staticRenderFns__ = [];
       component.staticRenderFns = template.staticRenderFns;
       component._compiled = true;
 
-      if (functional) component.functional = true;
+      if (functional) { component.functional = true; }
     }
 
     component._scopeId = scope;
-
-    {
-      let hook;
-      {
-        // In SSR.
-        hook = function(context) {
-          // 2.3 injection
-          context =
-            context || // cached call
-            (this.$vnode && this.$vnode.ssrContext) || // stateful
-            (this.parent && this.parent.$vnode && this.parent.$vnode.ssrContext); // functional
-          // 2.2 with runInNewContext: true
-          if (!context && typeof __VUE_SSR_CONTEXT__ !== 'undefined') {
-            context = __VUE_SSR_CONTEXT__;
-          }
-          // inject component styles
-          if (style) {
-            style.call(this, createInjectorSSR(context));
-          }
-          // register component module identifier for async chunk inference
-          if (context && context._registeredComponents) {
-            context._registeredComponents.add(moduleIdentifier);
-          }
-        };
-        // used by ssr in case component is cached and beforeCreate
-        // never gets called
-        component._ssrRegister = hook;
-      }
-
-      if (hook !== undefined) {
-        if (component.functional) {
-          // register for functional component in vue file
-          const originalRender = component.render;
-          component.render = function renderWithStyleInjection(h, context) {
-            hook.call(context);
-            return originalRender(h, context)
-          };
-        } else {
-          // inject component registration as beforeCreate hook
-          const existing = component.beforeCreate;
-          component.beforeCreate = existing ? [].concat(existing, hook) : [hook];
-        }
-      }
-    }
 
     return component
   }
   /* style inject */
   
   /* style inject SSR */
-  function __vue_create_injector_ssr__(context) {
-    if (!context && typeof __VUE_SSR_CONTEXT__ !== 'undefined') {
-      context = __VUE_SSR_CONTEXT__;
-    }
-
-    if (!context) return function () {}
-
-    if (!context.hasOwnProperty('styles')) {
-      Object.defineProperty(context, 'styles', {
-        enumerable: true,
-        get: () => context._styles
-      });
-      context._renderStyles = renderStyles;
-    }
-
-    function renderStyles(styles) {
-      let css = '';
-      for (const {ids, media, parts} of styles) {
-        css +=
-          '<style data-vue-ssr-id="' + ids.join(' ') + '"' + (media ? ' media="' + media + '"' : '') + '>'
-          + parts.join('\n') +
-          '</style>';
-      }
-
-      return css
-    }
-
-    return function addStyle(id, css) {
-      const group = css.media || 'default';
-      const style = context._styles[group] || (context._styles[group] = { ids: [], parts: [] });
-
-      if (!style.ids.includes(id)) {
-        style.media = css.media;
-        style.ids.push(id);
-        let code = css.source;
-        style.parts.push(code);
-      }
-    }
-  }
+  
 
   
   var SkyVideo = __vue_normalize__(
@@ -177,10 +90,10 @@ var __vue_staticRenderFns__ = [];
     __vue_is_functional_template__,
     __vue_module_identifier__,
     undefined,
-    __vue_create_injector_ssr__
+    undefined
   );
 
-const defaults = {
+var defaults = {
 	registerComponents: true,
 };
 
@@ -189,7 +102,8 @@ function install(Vue, options) {
 		return;
 	}
 
-	const { registerComponents } = Object.assign({}, defaults, options);
+	var ref = Object.assign({}, defaults, options);
+	var registerComponents = ref.registerComponents;
 
 	if (registerComponents) {
 		// Main component
